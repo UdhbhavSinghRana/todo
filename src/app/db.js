3b336a0050b2cae117c5b0cf4c90f54bc39b2b08
Udhbhav <21889udbhav@gmail.com>
2023-06-12 20:03:25 +0530

    Delete boilerplate

---
+++ src/app/db.js
@@ -0,0 +1,12 @@
+const { PrismaClient } = require("@prisma/client");
+
+const globalForPrisma = global;
+globalForPrisma.prisma = globalForPrisma.prisma || new PrismaClient({
+  log: ["query"],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = globalForPrisma.prisma;
+}
+
+module.exports = globalForPrisma.prisma;
